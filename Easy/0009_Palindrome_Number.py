59e5ccfe55f9903ed8d6c0b4129af2050b8a3370
mwall-dev <mwalchhuette@student.unimelb.edu.au>
2021-01-18 16:05:06 +1100

    Completed 0009

--- Easy/0009_Palindrome_Number.py
+++ Easy/0009_Palindrome_Number.py
@@ -1,23 +1,10 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        word = str(x)
+        return word == word[::-1]
 
 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.containsDuplicate([1,2,2,3,4]))
+    print(s.isPalindrome(123421))
