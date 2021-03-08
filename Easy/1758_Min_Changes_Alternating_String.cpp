fcf152dfe1ddbafbe5425e6730ac94d57f5571b7
mwall-dev <mwalchhuette@student.unimelb.edu.au>
2021-03-08 11:31:56 +1100

    Completed 1758

---
+++ Easy/1758_Min_Changes_Alternating_String.cpp
@@ -0,0 +1,50 @@
+/*
+
+    Approach 1: Brute force but doesnt work.
+        - Work your way along string and count swaps you make.
+        - return swaps
+        
+    Approach 2: slightly optimised
+        - Question asks for minimum. We may not get that.
+        e.g. 11110101100
+            if we start from index i and compare with i + 1.
+            we get 4 swaps 
+            
+            if we start from index i and compare with i - 1.
+            we get 7 swaps 
+            
+        We could iterate through twice and check for both answers and compare min.
+        
+        Basically we know end string will either be  0101010... or 101010... Starting with 1 or 0.
+        Try both ways and take min.
+    
+*/
+
+
+
+class Solution {
+public:
+    int minOperations(string s) {
+        
+        auto check_string = [](string s, char start) {
+            int swaps = 0;
+            if(s[0] != start) {
+                s[0] = start;
+                ++swaps;
+            }
+        
+            // Looking behind.
+            for(int i = 1; i < s.length(); ++i) {
+                if(s[i] == s[i-1]) {
+                    s[i] = s[i] == '0' ? '1' : '0';
+                    ++swaps;
+                }
+            }
+            return swaps;
+        };
+        
+            
+        return std::min(check_string(s, '0'), check_string(s, '1'));
+        
+    }
+};
